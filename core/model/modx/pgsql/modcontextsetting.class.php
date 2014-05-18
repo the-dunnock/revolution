30a9e500d36e9dc83089eb928a52cf2fbcec195e
Petri Rautiainen <the_dunnock@outlook.com>
2014-05-18 15:33:57 +0300

    Escapes

--- core/model/modx/pgsql/modcontextsetting.class.php
+++ core/model/modx/pgsql/modcontextsetting.class.php
@@ -12,20 +12,14 @@ class modContextSetting_pgsql extends modContextSetting {
     public static function listSettings(xPDO &$xpdo, array $criteria = array(), array $sort = array('id' => 'ASC'), $limit = 0, $offset = 0) {
         /* build query */
         $c = $xpdo->newQuery('modContextSetting');
-<<<<<<< HEAD
-        $c->select(array(
-            $xpdo->getSelectColumns('modContextSetting','modContextSetting'),
-=======
         $c->setClassAlias('modcontextsetting');
         $c->select(array(
             $xpdo->getSelectColumns('modContextSetting','modcontextsetting'),
->>>>>>> Loads of sql escapes, added override methods to pgsql objects
         ));
         $c->select(array(
             'entry.value AS name_trans',
             'description.value AS description_trans',
         ));
-<<<<<<< HEAD
         $c->leftJoin('modLexiconEntry','entry',"'setting_'||\"modContextSetting\".{$xpdo->escape('key')} = entry.name");
         $c->leftJoin('modLexiconEntry','description',"'setting_'||\"modContextSetting\".{$xpdo->escape('key')}||'_desc' = description.name");
         $c->where($criteria);
@@ -33,16 +27,6 @@ class modContextSetting_pgsql extends modContextSetting {
         $c->sortby($xpdo->getSelectColumns('modContextSetting','modContextSetting','',array('area')),'ASC');
         foreach($sort as $field=> $dir) {
             $c->sortby($xpdo->getSelectColumns('modContextSetting','modContextSetting','',array($field)),$dir);
-=======
-        $c->leftJoin('modLexiconEntry','entry',"'setting_'||modcontextsetting.{$xpdo->escape('key')}) = entry.name");
-        $c->leftJoin('modLexiconEntry','description',"'setting_'||modContextSetting.{$xpdo->escape('key')}||'_desc') = description.name");
-        $c->where($criteria);
-
-        $count = $xpdo->getCount('modContextSetting',$c);
-        $c->sortby($xpdo->getSelectColumns('modContextSetting','modcontextsetting','',array('area')),'ASC');
-        foreach($sort as $field=> $dir) {
-            $c->sortby($xpdo->getSelectColumns('modContextSetting','modcontextsetting','',array($field)),$dir);
->>>>>>> Loads of sql escapes, added override methods to pgsql objects
         }
         if ((int) $limit > 0) {
             $c->limit((int) $limit, (int) $offset);
